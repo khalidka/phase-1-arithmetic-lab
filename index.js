29f162bc202e6df69912f47cc8caa67f0321b8b9
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-12 11:33:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+// console.log(multiply(31, 2));
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+let max = Math.max(15, 10, 8, 13, 19, 20);
+console.log(max);
